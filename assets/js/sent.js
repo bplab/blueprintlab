50222e8c0f3c4c1118243849de43127933885383
BPLmarketer <marketer@blueprint-lab.com>
2020-07-30 13:24:32 +0900

    add form reset

--- assets/js/sent.js
+++ assets/js/sent.js
@@ -2,4 +2,5 @@ function myFunction(sent) {
   var x = document.getElementById("sent");
   x.className = "show";
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  setTimeout(function(){ document.gform.reset(); }, 3000);
 }
